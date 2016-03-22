c3825c1c2a2a5489173fb4c77a51eb1fa4b700d1
kolyan03 <kolyan03@yandex.ru>
2016-03-22 12:25:59 +0200

    Added files via upload

--- GoIT/js/Homework1-1/script/Homework1-1.js
+++ GoIT/js/Homework1-1/script/Homework1-1.js
@@ -1,30 +1,37 @@
 // JavaScript Document
 
 
-function pow() {
+pow(roundBaseNumber, roundGradeNumber);
 
-    alert('Функция возводит заданное пользователем число в заданную степень. Число должно быть целым, дробные значения будут округляться!');
+alert('Функция возводит заданное пользователем число в заданную степень. Число должно быть целым, дробные значения будут округляться!');
 
-    var baseNumber;
-    do {
-        baseNumber = prompt('Введите первое число', '');
-    } while (isNaN(baseNumber));
-    var roundBaseNumber = Math.round(baseNumber);
-    var gradeNumber;
-    do {
-        gradeNumber = prompt('Введите показатель степени', '');
-    } while (isNaN(gradeNumber));
-    var roundGradeNumber = Math.round(gradeNumber);
+var baseNumber;
+do {
+    baseNumber = prompt('Введите первое число', '');
+} while (isNaN(baseNumber));
+var roundBaseNumber = Math.round(baseNumber);
+var gradeNumber;
+do {
+    gradeNumber = prompt('Введите показатель степени', '');
+} while (isNaN(gradeNumber));
+var roundGradeNumber = Math.round(gradeNumber);
 
-    function makeAGrade() {
-        var result = 1;
+function pow(roundBaseNumber, roundGradeNumber) {
+    var result = 1;
+    if (roundGradeNumber >= 0) {
         for (var i = 0; i < roundGradeNumber; i++) {
             result = result * roundBaseNumber;
         }
         return result;
+    } else {
+        roundGradeNumber = Math.abs(roundGradeNumber);
+        for (var i = 0; i < roundGradeNumber; i++) {
+            result = result * roundBaseNumber;
+        }
+        return 1 / result;
     }
-    var calculationResult = makeAGrade(baseNumber, gradeNumber);
-    console.log('Результат возведения в степень равен: ' + calculationResult);
-    alert('Результат возведения в степень равен: ' + calculationResult);
 }
-pow();
+var calculationResult = pow(baseNumber, gradeNumber);
+console.log('Результат возведения в степень равен: ' + calculationResult);
+alert('Результат возведения в степень равен: ' + calculationResult);
+
