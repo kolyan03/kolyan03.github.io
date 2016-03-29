d037781947ec449bfc56631a6c76c27a9cfc48b3
kolyan03 <kolyan03@yandex.ru>
2016-03-29 14:17:56 +0300

    Transformed to object

--- GoIT/js/Homework 3/script/script.js
+++ GoIT/js/Homework 3/script/script.js
@@ -1,62 +1,65 @@
 //JavaScript document
-var jsWrapper = document.createElement('div');
-jsWrapper.classList.add('wrapper');
-var jsBody = document.querySelector('body');
-jsBody.appendChild(jsWrapper);
 
-var jsTopText = document.createElement('p');
-jsTopText.classList.add('top-text');
-jsWrapper.appendChild(jsTopText);
-jsTopText.innerText = 'Тест по программированию';
+var generateDOM = {
+    madeWrapper: function() {
+        jsWrapper = document.createElement('div');
+        jsWrapper.classList.add('wrapper');
+        document.body.appendChild(jsWrapper);
+    },
+    madeHeading: function() {
+        jsTopText = document.createElement('p');
+        jsTopText.classList.add('top-text');
+        jsWrapper.appendChild(jsTopText);
+        jsTopText.innerText = ' Тест по программированию ';
+    },
+    madeFormOfTest: function() {
+        jsForm = document.createElement('form');
+        jsWrapper.appendChild(jsForm);
+   
+        var howManyQuestions = 1;
+        do {
+            blockOfTest();
+            howManyQuestions++;
+        } while (howManyQuestions <= 5);
 
-var howManyQuestions = 1;
+        function blockOfTest() {
+            jsUl = document.createElement('ul');
+            jsUl.classList.add('aQuestion');
+            jsForm.appendChild(jsUl);
+            jsUl.innerHTML = howManyQuestions + '. Вопрос № ' + howManyQuestions;
 
-var jsForm = document.createElement('form');
-jsWrapper.appendChild(jsForm);
+            for (var i = 1; i <= 3; i++) {
 
-TEST();
+                var blockOfAnswer = document.createElement('div');
+                blockOfAnswer.classList.add('someAnswer');
+                jsForm.appendChild(blockOfAnswer);
 
-function TEST() {
+                var jsInput = document.createElement('input');
+                jsInput.type = 'checkbox';
+                jsInput.setAttribute('id', 'answer-number' + i);
+                blockOfAnswer.appendChild(jsInput);
 
-    do {
-        blockOfTest();
-        howManyQuestions++;
-    } while (howManyQuestions <= 3);
-}
+                var yourAnswer = document.createElement('label');
+                yourAnswer.setAttribute('for', 'answer-number' + i);
+                blockOfAnswer.appendChild(yourAnswer);
+                yourAnswer.innerHTML = 'Вариант ответа №' + i;
+            }
+        }
 
-function blockOfTest() {
+        var jsButton = document.createElement('input');
+        jsButton.type = 'submit';
+        jsButton.classList.add('confirm-answers');
+        jsForm.appendChild(jsButton);
+        jsButton.setAttribute('value', 'Проверить мои результаты ');
 
-    //Первый дочерний элемент- заглавие списка -
-    var jsUl = document.createElement('ul');
-    jsUl.classList.add('aQuestion');
-    jsForm.appendChild(jsUl);
-    jsUl.innerHTML = howManyQuestions + '. Вопрос № ' + howManyQuestions;
-
-    for (var i = 1; i <= 3; i++) {
-
-        //Затем - блок ответов
-        var blockOfAnswer = document.createElement('div');
-        blockOfAnswer.classList.add('someAnswer');
-        jsForm.appendChild(blockOfAnswer);
-
-        //В него влаживаем активное содержимое - input - 
-        var jsInput = document.createElement('input');
-        jsInput.type = 'checkbox';
-        jsInput.setAttribute('id', 'answer-number' + i);
-        blockOfAnswer.appendChild(jsInput);
-
-        //затем - метку
-        var yourAnswer = document.createElement('label');
-        yourAnswer.setAttribute('for', 'answer-number' + i);
-        blockOfAnswer.appendChild(yourAnswer);
-        yourAnswer.innerHTML = 'Вариант ответа №' + i;
+        console.log(document.documentElement);
     }
 }
 
-var jsButton = document.createElement('input');
-jsButton.type = 'submit';
-jsButton.classList.add('confirm-answers');
-jsForm.appendChild(jsButton);
-jsButton.setAttribute('value', 'Проверить мои результаты ');
+startTest();
 
-console.log(document.documentElement);
+function startTest() {
+    generateDOM.madeWrapper();
+    generateDOM.madeHeading();
+    generateDOM.madeFormOfTest();
+ };
