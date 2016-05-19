e41ea50870b9c31cc952f821b7aa2f88e1bbe5e7
kolyan03 <kolyan03@yandex.ru>
2016-05-19 13:43:55 +0300

    some bug fixed

--- GoIT/js/Homework7/script/js.js
+++ GoIT/js/Homework7/script/js.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $(function() {
 
-        $(".header li a").click(function() {
+        $('.header li a').click(function() {
             var $this = $(this);
             var $tabs = $(".header").find($this.attr('href'));
 
@@ -19,23 +19,32 @@ $(document).ready(function() {
                 $tabs.hide('slow');
                 $this.removeClass("active");
             }
-
             return false;
-
         });
-
     });
 
-    $('.enter-data').hover(function() {
-            var tipText = $(this).attr('tip');
-            var offset = $(this).offset();
-            $('.tooltip-block').html(tipText).show('slow');
-            $('.tooltip-block').offset({
-                left: offset.left + $(this).width() + $(window).scrollTop() +5,
-                top: offset.top
-            });
-        },
-        function() {
-            $('.tooltip-block').hide('fast');
+    $('.enter-data').on('mouseover', showTips);
+    $('.enter-data').on('mouseout', hideTips);
+    $('.show-tips').on('click', seeHelp);
+
+    function seeHelp() {
+        $('span').toggleClass('tips');
+
+    };
+
+    function showTips() {
+        var tipText = $(this).attr('tip');
+        var offset = $(this).offset();
+        $('.tooltip-block').html(tipText).show('slow');
+        $('.tooltip-block').offset({
+            left: offset.left + $(this).width() + $(window).scrollTop() + 5,
+            top: offset.top
         });
+        console.log(this);
+    };
+
+    function hideTips() {
+        $('.tooltip-block').hide('fast');
+    };
+
 });
